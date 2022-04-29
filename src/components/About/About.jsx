e191631b2957c9583d8237cb5fdcab8b439912bb
Damjan Ravnjak <damjan@24setup.com>
2022-04-29 17:46:35 +0200

    chore: Clean up application component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+    return (
+        <div>
+            <h2>Our Mission</h2>
+            <p className="opacity-75">
+                Dedication to your success.
+            </p>
+            <h2>Our Process</h2>
+            <p className="opacity-75">
+                Before creation, there's thinking.<br/>
+                Our process is sharp and let us craft the best quality.
+            </p>
+        </div>
+    )
+}
+
+export default About

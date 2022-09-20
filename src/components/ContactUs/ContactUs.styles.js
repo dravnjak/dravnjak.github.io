f7022fe6d3b9c63878290a457607bfa11b7784dc
Ravnjak Marijana <you@example.com>
2022-09-20 19:24:29 +0200

    feat(#26): Update ContactUs component to use styles

---
+++ src/components/ContactUs/ContactUs.styles.js
@@ -0,0 +1,18 @@
+export default {
+  bottomArea: {
+    position: 'absolute',
+    bottom: '1rem',
+    right: '1rem',
+
+    '& .h3, $ .p': {
+      margin: '0'
+    },
+  },
+
+  '@media (min-width: 768px)': {
+    bottomArea: {
+      bottom: '3rem',
+      right: '3rem'
+    }
+  }
+}

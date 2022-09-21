2360180d6ee784bc1da2b542d352031346df4368
Ravnjak Marijana <you@example.com>
2022-09-21 11:40:16 +0200

    feat(#26): Correction ContactUs component

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -11,8 +11,8 @@ const ContactUs = () => {
 
   return(
     <div className={classes.bottomArea}>
-      <h3>Contact us</h3>
-      <p className={classes.opacity75}>
+      <h3 className={classes.h3}>Contact us</h3>
+      <p className={classes.p}>
         <a href="mailto:damjan@24setup.com">office@24setup.com</a>
       </p>
     </div>

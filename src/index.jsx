d8248b298ab6fd22665c758be50ee0f633745e25
Ravnjak Marijana <you@example.com>
2022-09-22 09:53:05 +0200

    feat(#32): Changes index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import Application from './Application'
+
+import Home from 'Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Application />
+    <Home />
   </React.StrictMode>
 )
 
